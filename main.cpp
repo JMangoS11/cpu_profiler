ee805e51f040bf0b8ccc8860cbfa864b9df59948
FarWrong <morewrong32@gmail.com>
2023-02-15 14:49:50 -0500

    Synchronized sleep/wake
    sychrnoized sleep/wake

--- main.cpp
+++ main.cpp
@@ -8,12 +8,16 @@
 using namespace std::chrono;
 void* sleep_thread(void * arg);
 void* run_computation(void * arg);
+// our global condition variable and mutex
+pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
 
+int sleep_length = 3;
+
+int initialized = 0;
 struct thread_args {
   int id;
-  int seconds_to_sleep = 3;
-  float sleep_ratio = 0.25;
-  int millisecond_duration = 10000;
+  int test_duration = 500;
   int * value;
 };
 
@@ -28,12 +32,13 @@ int main()
   CPU_ZERO(&cpuset);
   pthread_t thread_array[num_threads];
 
+  pthread_mutex_unlock(&mtx);
   //create all the threads
   for (int i = 0; i < num_threads; i++) {
     struct thread_args *args = new struct thread_args;
 
     //decide which cores to bind cpus too
-    CPU_SET(i % 2 * 2, &cpuset);
+    CPU_SET(i , &cpuset);
 
     //give an id and the reference to the sum_value to all threads
     args->id = i;
@@ -42,6 +47,15 @@ int main()
     pthread_create(&thread_array[i], NULL, run_computation, (void *) args);
     pthread_setaffinity_np(thread_array[i], sizeof(cpu_set_t), &cpuset);
 
+  }
+
+  while(true){
+    printf("sleeping \n");
+    sleep(sleep_length);
+    initialized = 1;
+    printf("awakening \n");
+    pthread_cond_broadcast(&cv);
+    pthread_mutex_unlock(&mtx);
   }
 
   //join the threads
@@ -56,28 +70,37 @@ int main()
 
 void* run_computation(void * arg)
 {
+
     struct thread_args *args = (struct thread_args *)arg;
-    printf("Starting computation on Thread:%d\n",args->id);
+    while(true){
+      pthread_mutex_lock(&mtx);
+      initialized = 0;
+      while (! initialized) {
+      pthread_cond_wait(&cv, &mtx);
+      }
+      pthread_mutex_unlock(&mtx);
+      
+      int addition_calculator = 0;
 
-    int addition_calculator = 0;
-
-    //calculating the end time with duration + current time
-    auto end = high_resolution_clock::now() + std::chrono::milliseconds(args -> millisecond_duration);
+      //calculating the end time with duration + current time
+      auto end = high_resolution_clock::now() + std::chrono::milliseconds(args -> test_duration);
 
 
-    
-    //checking that right now is before the end time
-    while(std::chrono::high_resolution_clock::now() < end){
-      *args->value += 1;
-      addition_calculator += 1;S
+      int ms = duration_cast< milliseconds >(
+    system_clock::now().time_since_epoch()
+).count();
+      std::cout << "Thread " << args->id << " starts at :" << ms % 1000;
+      //checking that right now is before the end time
+      while(std::chrono::high_resolution_clock::now() < end){
+        *args->value += 1;
+        addition_calculator += 1;
+      }
+      ms = duration_cast< milliseconds >(
+    system_clock::now().time_since_epoch()
+).count() ;
+      std::cout << "Thread " << args->id << " ends at :" << ms % 1000;
+      std::cout << "Thread " << args->id << " Current cpu: " << sched_getcpu() ;
+      printf(" Core Speed:%d \n", addition_calculator / args -> test_duration );
     }
-
-    long sleep_length = args -> millisecond_duration * args ->sleep_ratio;
-    std::cout << "Thread " << args->id << " Current cpu: " << sched_getcpu() ;
-    printf(" Core Speed:%d \n", addition_calculator * 2 / args ->millisecond_duration );
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(sleep_length));
-
-    printf("Thread returns final computation:%d\n",*args->value);
     return NULL;
 }
