a327669f41b3eefae844f9c2054292d25cb37c90
FarWrong <morewrong32@gmail.com>
2023-02-21 17:55:18 -0500

    change list to mutex

--- main.cpp
+++ main.cpp
@@ -6,56 +6,71 @@
 #include <thread>
 
 using namespace std::chrono;
+
 void* sleep_thread(void * arg);
 void* run_computation(void * arg);
+
 // our global condition variable and mutex
 pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
 
-int sleep_length = 3;
-
+int sleep_length =1;
 int initialized = 0;
+
 struct thread_args {
   int id;
   int test_duration = 500;
-  int * value;
+  int start_time = 0;
+  int end_time = 0;
+  float speed = 0;
+  pthread_mutex_t mutex;
 };
 
 int main() 
 {
 
   int num_threads = 4;
+
   cpu_set_t cpuset;
-  //value to be added up by threads
-  int sum_value = 0;
-
   CPU_ZERO(&cpuset);
+  
   pthread_t thread_array[num_threads];
+  pthread_mutex_t mutex_array[num_threads];
+  struct thread_args* args_array[num_threads];
 
-  pthread_mutex_unlock(&mtx);
-  //create all the threads
+
+  //create all the threads and initilize mutex
   for (int i = 0; i < num_threads; i++) {
     struct thread_args *args = new struct thread_args;
-
+    
+    //init mutex
+    mutex_array[i] =  PTHREAD_MUTEX_INITIALIZER;
     //decide which cores to bind cpus too
     CPU_SET(i , &cpuset);
 
-    //give an id and the reference to the sum_value to all threads
+    //give an id to all threads
     args->id = i;
-    args->value = &sum_value;
+    args->mutex = mutex_array[i];
+
 
     pthread_create(&thread_array[i], NULL, run_computation, (void *) args);
     pthread_setaffinity_np(thread_array[i], sizeof(cpu_set_t), &cpuset);
-
+    args_array[i] = args;
   }
+
+
 
   while(true){
     printf("sleeping \n");
     sleep(sleep_length);
     initialized = 1;
+
+    //print speed from LAST run
+    for (int i = 0; i < num_threads; i++) {
+      std::cout<< "Thread:"<<args_array[i]->id << " Speed:" <<args_array[i]->speed <<" Computation_start:"<<args_array[i]->start_time % 10000 << " Computation_end:" << args_array[i]->end_time % 10000 << std::endl ;
+
+    };
     printf("awakening \n");
     pthread_cond_broadcast(&cv);
-    pthread_mutex_unlock(&mtx);
   }
 
   //join the threads
@@ -63,7 +78,7 @@ int main()
     pthread_join(thread_array[i], NULL);
   }
 
-  printf("Finished, computed value:%d",sum_value);
+  printf("Process Finished");
   return 0;
 }
 
@@ -73,12 +88,12 @@ void* run_computation(void * arg)
 
     struct thread_args *args = (struct thread_args *)arg;
     while(true){
-      pthread_mutex_lock(&mtx);
+      pthread_mutex_lock(&args->mutex);
       initialized = 0;
       while (! initialized) {
-      pthread_cond_wait(&cv, &mtx);
+      pthread_cond_wait(&cv, &args->mutex);
       }
-      pthread_mutex_unlock(&mtx);
+      pthread_mutex_unlock(&args->mutex);
       
       int addition_calculator = 0;
 
@@ -86,21 +101,15 @@ void* run_computation(void * arg)
       auto end = high_resolution_clock::now() + std::chrono::milliseconds(args -> test_duration);
 
 
-      int ms = duration_cast< milliseconds >(
-    system_clock::now().time_since_epoch()
-).count();
-      std::cout << "Thread " << args->id << " starts at :" << ms % 1000;
+      int ms = duration_cast< milliseconds >(system_clock::now().time_since_epoch()).count();
+      args->start_time = ms;
       //checking that right now is before the end time
       while(std::chrono::high_resolution_clock::now() < end){
-        *args->value += 1;
         addition_calculator += 1;
       }
-      ms = duration_cast< milliseconds >(
-    system_clock::now().time_since_epoch()
-).count() ;
-      std::cout << "Thread " << args->id << " ends at :" << ms % 1000;
-      std::cout << "Thread " << args->id << " Current cpu: " << sched_getcpu() ;
-      printf(" Core Speed:%d \n", addition_calculator / args -> test_duration );
+      ms = duration_cast< milliseconds >(system_clock::now().time_since_epoch()).count() ;
+      args->end_time = ms;
+      args->speed = addition_calculator / args -> test_duration;
     }
     return NULL;
 }
