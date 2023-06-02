64991366c1074cb4e77fbdb8fd3fbee31c2d4cb1
FarWrong <morewrong32@gmail.com>
2023-06-02 15:12:32 -0400

    asdf
    asdfasdf

--- main.cpp
+++ main.cpp
@@ -157,7 +157,7 @@ bool set_pthread_to_low_prio_cgroup(pthread_t thread) {
 
 
 //To get steal time of ALL CPUs
-void get_steal_time_all(int cpunum,std::array<raw_data>& steal_arr){
+void get_steal_time_all(int cpunum,std::vector<raw_data>& steal_arr){
   std::ifstream f("/proc/stat");
   std::string s;
   int output[cpunum];
@@ -174,7 +174,7 @@ void get_steal_time_all(int cpunum,std::array<raw_data>& steal_arr){
 }
 
 //get preemptions of ALL cpus
-void get_preempts_all(int cpunum, std::array<raw_data>& preempt_arr) {
+void get_preempts_all(int cpunum, std::vector<raw_data>& preempt_arr) {
     std::ifstream f("/proc/preempts");
     std::string s;
     int x;
@@ -189,7 +189,7 @@ void get_preempts_all(int cpunum, std::array<raw_data>& preempt_arr) {
 
 
 //get run time of ALL cpus
-void get_run_time_all(int cpunum,std::array<raw_data>& run_arr){
+void get_run_time_all(int cpunum,std::vector<raw_data>& run_arr){
   std::ifstream f("/proc/stat");
   std::string s;
   int output[cpunum];
@@ -264,9 +264,11 @@ int main(int argc, char *argv[]) {
   struct thread_args* args_array[num_threads];
 
 
-  std::array<raw_data, num_threads> data_begin;
-  std::array<raw_data, num_threads> data_end;
-  std::array<profiled_data, num_threads> result_data;
+  std::vector<raw_data> data_begin;
+  std::vector<raw_data> data_end;
+  data_begin.resize(num_threads);
+  data_end.resize(num_threads);
+  profiled_data result_data[num_threads];
 
   std::deque<std::vector<int>> steal_history;
   pthread_t thId = pthread_self();
