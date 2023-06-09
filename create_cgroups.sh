afca704aca6a096d5963659e9c2d228d534a4b64
FarWrong <morewrong32@gmail.com>
2023-06-09 14:40:20 -0400

    new shell command

--- create_cgroups.sh
+++ create_cgroups.sh
@@ -2,29 +2,17 @@ if ! grep -q "cpu" /sys/fs/cgroup/cgroup.subtree_control; then
     sudo echo "+cpu" >> /sys/fs/cgroup/cgroup.subtree_control; 
 fi 
 
-
-
-if [ ! -d /sys/fs/cgroup/par_cgroup ]; then
-    sudo mkdir /sys/fs/cgroup/par_cgroup
+if [ ! -d /sys/fs/cgroup/hi_prgroup ]; then
+    sudo mkdir /sys/fs/cgroup/hi_prgroup
 fi
 
-if ! grep -q "threaded" /sys/fs/cgroup/par_cgroup/cgroup.type; then 
-    sudo echo "threaded" > /sys/fs/cgroup/par_cgroup/cgroup.type; 
-fi 
 
-if ! grep -q "cpu" /sys/fs/cgroup/par_cgroup/cgroup.subtree_control; then 
-    sudo echo "+cpu" >> /sys/fs/cgroup/par_cgroup/cgroup.subtree_control; 
-fi 
-
-if [ ! -d /sys/fs/cgroup/par_cgroup/hi_prgroup ]; then
-    sudo mkdir /sys/fs/cgroup/par_cgroup/hi_prgroup
+if [ ! -d /sys/fs/cgroup/lw_prgroup ]; then
+    sudo mkdir /sys/fs/cgroup/lw_prgroup
 fi
 
-if [ ! -d /sys/fs/cgroup/par_cgroup/lw_prgroup ]; then
-    sudo mkdir /sys/fs/cgroup/par_cgroup/lw_prgroup
-fi
 
-sudo echo "threaded" > /sys/fs/cgroup/par_cgroup/lw_prgroup/cgroup.type
-sudo echo "threaded" > /sys/fs/cgroup/par_cgroup/hi_prgroup/cgroup.type
-sudo echo 1 > /sys/fs/cgroup/par_cgroup/lw_prgroup/cpu.idle
-sudo echo -20 > /sys/fs/cgroup/par_cgroup/hi_prgroup/cpu.weight.nice
+sudo echo "threaded" > /sys/fs/cgroup/lw_prgroup/cgroup.type; 
+sudo echo "threaded" > /sys/fs/cgroup/hi_prgroup/cgroup.type; 
+sudo echo 1 > /sys/fs/cgroup/lw_prgroup/cpu.idle
+sudo echo -20 > /sys/fs/cgroup/hi_prgroup/cpu.weight.nice
