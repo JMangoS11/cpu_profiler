44bfc266fa86d86fc32bd438b72655837bb38802
JMangoS <justmarg11@gmail.com>
2024-08-12 11:53:38 -0400

    Everything that was changed since july

--- create_cgroups.sh
+++ create_cgroups.sh
@@ -1,7 +1,7 @@
 if ! grep -q "cpu" /sys/fs/cgroup/cgroup.subtree_control; then 
     sudo echo "+cpu+cpuset" >> /sys/fs/cgroup/cgroup.subtree_control; 
-    sudo echo "+cpuset">> /sys/fs/cgroup/cgroup.subtree_control;
 fi
+sudo echo "+cpuset">> /sys/fs/cgroup/cgroup.subtree_control;
 
 if [ ! -d /sys/fs/cgroup/hi_prgroup ]; then
     sudo mkdir /sys/fs/cgroup/hi_prgroup
@@ -16,4 +16,4 @@ fi
 sudo echo "threaded" > /sys/fs/cgroup/lw_prgroup/cgroup.type; 
 sudo echo "threaded" > /sys/fs/cgroup/hi_prgroup/cgroup.type; 
 sudo echo 1 > /sys/fs/cgroup/lw_prgroup/cpu.idle
-sudo echo 20 > /sys/fs/cgroup/hi_prgroup/cpu.weight.nice
+sudo echo 19 > /sys/fs/cgroup/hi_prgroup/cpu.weight.nice
--- setup_vcapacity.sh
+++ setup_vcapacity.sh
@@ -1,7 +1,11 @@
-sudo bash /home/ubuntu/cpu_profiler/create_cgroups.sh
-sudo insmod /home/ubuntu/vsched/custom_modules/preempt_proc.ko
-sudo insmod /home/ubuntu/vsched/custom_modules/cust_capacity.ko
-sudo insmod /home/ubuntu/vsched/custom_modules/max_latency.ko
-
+sudo sh /home/jmangos/cpu_profiler/create_cgroups.sh
+sudo rmmod preempt_proc
+sudo rmmod cust_capacity
+sudo rmmod max_latency
+sudo rmmod cust_topo
+sudo insmod /home/jmangos/vsched/custom_modules/preempt_proc.ko
+sudo insmod /home/jmangos/vsched/custom_modules/cust_capacity.ko
+sudo insmod /home/jmangos/vsched/custom_modules/max_latency.ko
+sudo insmod /home/jmangos/vsched/custom_modules/cust_topo.ko
 
 
